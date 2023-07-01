5e5ba264ba178c3e5d56240114aef356557d0236
pogodins <pogodin.serg@gmail.com>
2023-07-01 16:06:07 +0200

    Add Playwright component tests

---
+++ src/tests/NoImages.spec.jsx
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import NoImages from '../components/NoImages';
+
+test('No images error is shown', async ({ mount }) => {
+    const noImagesComponent = await mount(<NoImages />);
+
+    const noImagesTitle = noImagesComponent.getByText('No Images Found');
+    const tryLater = noImagesComponent.getByText('Please try a different search term');
+    await expect(noImagesTitle).toBeVisible();
+    await expect(tryLater).toBeVisible();
+});
